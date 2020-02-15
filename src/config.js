ff701cd91bfb890a71245c06f78b2a469d7816a0
Lewis Monteith <lewismonteith@gmail.com>
2020-02-15 10:08:04 +0000

    Remove hard coded url in favour of ssm param

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-  baseApiUri: process.env.base_api_url || 'https://wodmfays48.execute-api.eu-west-2.amazonaws.com/test'
+  baseApiUri: process.env.base_api_url
 };
